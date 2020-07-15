9aa9152ed7c77972f5cc7f3548a2e369929b447c
mharrvic <mharrvic@gmail.com>
2020-07-16 01:42:32 +0800

    roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        body: ["Roboto", "sans-serif"],
+      },
+    },
   },
   variants: {},
   plugins: [],
